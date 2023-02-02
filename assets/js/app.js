e93e69edd517cebd03b74a82c507f8bebcdb581d
sebastiansev <sebastianseverianodev@gmail.com>
2023-02-02 13:44:13 -0600

    menu dark mode

---
+++ assets/js/app.js
@@ -0,0 +1,26 @@
+const btnMenu = document.getElementById("btn-menu");
+let headerMenu = document.querySelector(".box-menu");
+let btnTheme = document.querySelector(".btn-theme");
+
+
+
+function toggleMenu(){
+    let linetop = document.querySelector(".line-1"),
+    linecenter = document.querySelector(".line-2"),
+    lineBtn = document.querySelector(".line-3");
+
+    headerMenu.classList.toggle("active");
+    linetop.classList.toggle("active");
+    linecenter.classList.toggle("active");
+    lineBtn.classList.toggle("active");
+}
+
+function changeTheme(){
+    let body = document.querySelector("body");
+    btnTheme.classList.toggle("active")
+
+    body.classList.toggle("theme-dark");
+}
+
+btnMenu.addEventListener("click",toggleMenu)
+btnTheme.addEventListener("click",changeTheme)
